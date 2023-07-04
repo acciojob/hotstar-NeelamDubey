e6f493ebf6ae5c99776b507a28f73b59080a5de1
Neelam Dubey <ashidubey8208@gmail.com>
2023-07-04 13:51:52 +0530

    Second try

--- src/main/java/com/driver/services/ProductionHouseService.java
+++ src/main/java/com/driver/services/ProductionHouseService.java
@@ -15,7 +15,10 @@ public class ProductionHouseService {
 
     public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
 
-        return  null;
+        ProductionHouse productionHouse= new ProductionHouse(productionHouseEntryDto.getName());
+        productionHouse= productionHouseRepository.save(productionHouse);
+
+        return  productionHouse.getId();
     }
 
 
